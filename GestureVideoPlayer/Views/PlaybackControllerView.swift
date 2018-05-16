722ae76885a0b5ea2168c482fb1d807bfa1d5ca0
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-05-16 20:40:56 +0900

    detect pan direction and seek time by panGesture

--- GestureVideoPlayer/Views/PlaybackControllerView.swift
+++ GestureVideoPlayer/Views/PlaybackControllerView.swift
@@ -24,16 +24,10 @@ class PlaybackControllerView: UIView {
             playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .selected)
         }
     }
-    @IBOutlet weak var enlargeShrinkButton: UIButton! {
-        didSet{
-            enlargeShrinkButton.setImage(#imageLiteral(resourceName: "fill"), for: .normal)
-            enlargeShrinkButton.setImage(#imageLiteral(resourceName: "shrink"), for: .selected)
-        }
-    }
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var playbackSlider: UISlider! {
-        didSet{
+        didSet {
             let image = UIImage(cgImage: #imageLiteral(resourceName: "thumbImage").cgImage!, scale: 4, orientation: UIImageOrientation.up)
             playbackSlider.setThumbImage(image, for: .normal)
         }
@@ -71,17 +65,5 @@ class PlaybackControllerView: UIView {
         let isSelected = !playPauseButton.isSelected
         playPauseButton.isSelected = isSelected
         delegate?.playbackControllerView(toBePlay: isSelected)
-    }
-    @IBAction func enlargeShrinkButtonTapped(_ sender: Any) {
-        let value:Int!
-        if isLandscapeMode {
-            value = UIInterfaceOrientation.portrait.rawValue
-            isLandscapeMode = false
-        }else {
-            value = UIInterfaceOrientation.landscapeRight.rawValue
-            isLandscapeMode = true
-        }
-        enlargeShrinkButton.isSelected = isLandscapeMode
-        UIDevice.current.setValue(value, forKey: "orientation")
     }
 }
